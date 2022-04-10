1ebc4c5266c8f3c39dff80a9c54a7c38045da43b
Tamara Wilhite <wilhite.tamara6452@gmail.com>
2022-04-10 13:52:15 -0400

    added links and icons to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,34 @@
 import React from "react";
 import "./style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+
+library.add(fab);
 
 function Footer() {
   return (
-    <footer>
-      <h3>Made with &hearts; by Tamara.</h3>
+    <footer className="border-top">
+      <nav className="d-flex flex-row justify-content-center">
+        <a
+          className="text-decoration-none icon py-2 px-4"
+          href="https://github.com/trwilhite"
+        >
+          <FontAwesomeIcon icon={["fab", "github"]} size="2x" />{" "}
+        </a>
+        <a
+          className="text-decoration-none icon py-2 px-4"
+          href="https://www.linkedin.com/in/tamara-wilhite/"
+        >
+          <FontAwesomeIcon icon={["fab", "linkedin"]} size="2x" />{" "}
+        </a>
+        <a
+          className="text-decoration-none icon py-2 px-4"
+          href="https://stackoverflow.com/users/17314835/tamara-wilhite"
+        >
+          <FontAwesomeIcon icon="fa-brands fa-stack-overflow" size="2x" />{" "}
+        </a>
+      </nav>
     </footer>
   );
 }
